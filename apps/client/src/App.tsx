14f01a54136b2f2c4290c8123164e95c90a37a16
superfrost <otkycu@gmail.com>
2022-07-09 16:27:23 +0300

    init

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    getData()
+  }, [])
+  
+  const getData = async () => {
+    const result = await fetch('http://localhost:3000')
+    const data = await result.json()
+    setMessage(data?.message)
+  }
   return (
     <div>
       <h1>Hello</h1>
+      <h2>{message}</h2>
     </div>
   )
 }
